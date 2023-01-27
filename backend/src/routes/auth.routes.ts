38b4f3d87147a4986a408a892f1bacfaed287429
luiggy macias <ferrinluiggy@gmail.com>
2023-01-27 18:18:14 -0500

    se agrega bcryt verificar contrasena

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -6,7 +6,7 @@ import bcrypt from "bcryptjs";
 
 const router = Router();
 
-const loginValidation = {
+const registerValidation = {
   body: Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string()
@@ -16,61 +16,89 @@ const loginValidation = {
     nombre: Joi.string().required(),
   }),
 };
+
+const loginValidation = {
+  body: Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string()
+      .regex(/[a-zA-Z0-9]{3,30}/)
+      .required(),
+  }),
+};
+
+type User = {
+  id: number;
+  nombre: string;
+  username: string;
+  email: string;
+  password: string;
+};
 router.post("/login", validate(loginValidation, {}, {}), async (req, res) => {
   try {
     // buscar el usuario en la base de datos usango orm sequelize
     const { email, password } = req.body;
-    const user = await Usuarios.findAll({
-      attributes: ["id", "nombre", "username", "email"],
+    const user: any = await Usuarios.findOne({
+      attributes: ["id", "nombre", "username", "email", "password"],
       where: {
         email,
-        password,
       },
     });
-    if (user.length === 0) {
+    // return res.json(user)
+    if (!user) {
       throw new Error("Usuario no encontrado");
+    }
+    // verificamos que la contraseña sea correcta
+    const matchPassword = await bcrypt.compare(password, user.password);
+    if (!matchPassword) {
+      throw new Error("Contraseña incorrecta");
     }
     res.json({
       user,
     });
   } catch (error: any) {
+    console.log(error);
+    
     res.status(404).json({
       error: error.message,
     });
   }
 });
 
-router.post("/signup", validate(loginValidation, {}, {}), async (req, res) => {
-  try {
-    // crear el usuario en la base de datos usango orm sequelize
-    const { email, password, nombre, username } = req.body;
-    // verificamos que no exista el usuario
-    const userFound = await Usuarios.findAll({
-      attributes: ["id", "nombre", "username", "email"],
-      where: {
+router.post(
+  "/signup",
+  validate(registerValidation, {}, {}),
+  async (req, res) => {
+    try {
+      // crear el usuario en la base de datos usango orm sequelize
+      const { email, password, nombre, username } = req.body;
+      // verificamos que no exista el usuario
+      const userFound = await Usuarios.findAll({
+        attributes: ["id", "nombre", "username", "email"],
+        where: {
+          email,
+        },
+      });
+      if (userFound.length > 0) {
+        throw new Error("Usuario ya existe");
+      }
+      const encryptedPassword = bcrypt.hashSync(password, 10);
+      const user = await Usuarios.create({
         email,
-      },
-    });
-    if (userFound.length > 0) {
-      throw new Error("Usuario ya existe");
+        password: encryptedPassword,
+        nombre,
+        username,
+      });
+      res.json({
+        message: "Usuario creado",
+        user,
+      });
+    } catch (error: any) {
+      console.log("error");
+      res.status(404).json({
+        error: error.message,
+      });
     }
-    const encryptedPassword = bcrypt.hashSync(password, 10);
-    const user = await Usuarios.create({
-      email,
-      password: encryptedPassword,
-      nombre,
-      username,
-    });
-    res.json({
-      message: "Usuario creado",
-      user,
-    });
-  } catch (error: any) {
-    console.log("error");
-    res.status(404).json({
-      error: error.message,
-    });
   }
-});
+);
 
 export default router;
