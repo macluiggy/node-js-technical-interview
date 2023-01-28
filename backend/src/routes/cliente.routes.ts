458e75f9be47da56ce64d49dd6a5ed3af7b08627
luiggy macias <ferrinluiggy@gmail.com>
2023-01-27 19:14:46 -0500

    se agrega crud de cliente

--- backend/src/routes/cliente.routes.ts
+++ backend/src/routes/cliente.routes.ts
@@ -32,5 +32,94 @@ router.post("/cliente", verifyToken, async (req, res) => {
   }
 });
 
+router.get("/cliente", verifyToken, async (req, res) => {
+  try {
+    const clientes = await Cliente.findAll();
+    return res.json({
+      data: clientes,
+    });
+  } catch (error: any) {
+    console.log(error);
+    
+    res.status(404).json({
+      error: error.message,
+    });
+  }
+});
+
+router.get("/cliente/:id", verifyToken, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const cliente = await Cliente.findOne({
+      where: {
+        id,
+      },
+    });
+    return res.json({
+      data: cliente,
+    });
+  } catch (error: any) {
+    console.log(error);
+
+    res.status(404).json({
+      error: error.message,
+    });
+  }
+})
+
+router.put("/cliente/:id", verifyToken, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { ncedula, nombre, apellido, id_departamento } = req.body;
+    const cliente = await Cliente.findOne({
+      where: {
+        id,
+      },
+    });
+    if (cliente) {
+      await cliente.update({
+        ncedula,
+        nombre,
+        apellido,
+        id_departamento,
+      });
+      return res.json({
+        message: "Cliente actualizado",
+        data: cliente,
+      });
+    }
+  } catch (error: any) {
+    console.log(error);
+
+    res.status(404).json({
+      error: error.message,
+    });
+    
+  }
+})
+
+router.delete("/cliente/:id", verifyToken, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const cliente = await Cliente.findOne({
+      where: {
+        id,
+      },
+    });
+    if (cliente) {
+      await cliente.destroy();
+      return res.json({
+        message: "Cliente eliminado",
+        data: cliente,
+      });
+    }
+  } catch (error: any) {
+    console.log(error);
+    res.status(404).json({
+      error: error.message,
+    });
+  }
+})
+
 
 export default router;
